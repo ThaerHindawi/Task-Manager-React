eaabb854b81f6edee36284e52340818f8c1447f4
Thaer Hendawi <thaerhindawi55@gmail.com>
2025-05-19 00:23:42 -0400

    add https

--- src/utils/API_URL.tsx
+++ src/utils/API_URL.tsx
@@ -4,5 +4,5 @@ export default function API_BASE_URL(param: string) {
   }
   if (import.meta.env.MODE === "development")
     return `http://127.0.0.1:8000/api/${param}`;
-  return `http://api.thaerhendawi.com/api/${param}`;
+  return `https://api.thaerhendawi.com/api/${param}`;
 }
